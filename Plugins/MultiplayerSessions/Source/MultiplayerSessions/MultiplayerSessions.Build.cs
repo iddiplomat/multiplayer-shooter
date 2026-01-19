e474c5d5a211c719d2ef97d4e3c2221dc05b2d0c
iddiplomat <irondoomdiplomat@gmail.com>
2026-01-19 18:19:32 +0300

    Create Menu widget.

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/MultiplayerSessions.Build.cs
@@ -8,50 +8,36 @@ public class MultiplayerSessions : ModuleRules
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicIncludePaths.AddRange(
-			new string[]
-			{
-				// ... add public include paths required here ...
-			}
-		);
+		PublicIncludePaths.AddRange([]);
 
 
-		PrivateIncludePaths.AddRange(
-			new string[]
-			{
-				// ... add other private include paths required here ...
-			}
-		);
+		PrivateIncludePaths.AddRange([]);
 
 
 		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
+			[
 				"Core",
 				"OnlineSubsystem",
 				"OnlineSubsystemSteam",
-				// ... add other public dependencies that you statically link with here ...
-			}
-		);
+				"UMG",
+				"Slate",
+				"SlateCore"
+		]);
 
 
 		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
+			[
 				"CoreUObject",
 				"Engine",
 				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
+				"SlateCore"
+			]
 		);
 
 
 		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
+			[
+			]
 		);
 	}
 }
