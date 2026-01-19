ffb4e4ff24b855cb845b1edd626226641ab56446
iddiplomat <irondoomdiplomat@gmail.com>
2026-01-19 18:46:06 +0300

    WIP menu.

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -1,5 +1,8 @@
 #include "Menu.h"
 
+#include "MultiplayerSessionsSubsystem.h"
+#include "Components/Button.h"
+
 void UMenu::MenuSetup()
 {
 	AddToViewport();
@@ -7,16 +10,59 @@ void UMenu::MenuSetup()
 	SetIsFocusable(true);
 	
 	UWorld* World = GetWorld();
-	if (World)
+	if (!World) return;
+	
+	APlayerController* PlayerController = World->GetFirstPlayerController();
+	if (!PlayerController) return;
+	
+	FInputModeUIOnly InputModeData;
+	InputModeData.SetWidgetToFocus(TakeWidget());
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	PlayerController->SetInputMode(InputModeData);
+	PlayerController->SetShowMouseCursor(true);
+	
+	UGameInstance* GameInstance = GetGameInstance();
+	if (!GameInstance) return;
+	
+	MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+}
+
+bool UMenu::Initialize()
+{
+	if (!Super::Initialize())
 	{
-		APlayerController* PlayerController = World->GetFirstPlayerController();
-		if (PlayerController)
-		{
-			FInputModeUIOnly InputModeData;
-			InputModeData.SetWidgetToFocus(TakeWidget());
-			InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
-			PlayerController->SetInputMode(InputModeData);
-			PlayerController->SetShowMouseCursor(true);
-		}
+		return false;
+	}
+	
+	if (HostButton)
+	{
+		HostButton->OnClicked.AddDynamic(this, &UMenu::HostButtonClicked);
+	}
+	
+	if (JoinButton)
+	{
+		JoinButton->OnClicked.AddDynamic(this, &UMenu::JoinButtonClicked);
+	}
+	
+	return true;
+}
+
+void UMenu::HostButtonClicked()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Blue, TEXT("Host Button Clicked"));
+	}
+	
+	if (!MultiplayerSessionsSubsystem) return;
+	
+	MultiplayerSessionsSubsystem->CreateSession(4, FString("FreeForAll"));
+}
+
+void UMenu::JoinButtonClicked()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Blue, TEXT("Join Button Clicked"));
 	}
 }
